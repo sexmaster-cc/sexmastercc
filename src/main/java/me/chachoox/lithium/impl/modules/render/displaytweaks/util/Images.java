d3514ec52b1b4e27977d9cb694076b2be71f44a6
pycnocarpa <soldfortheguap@protonmail.com>
2023-08-14 02:46:37 -0400

    new image

--- src/main/java/me/chachoox/lithium/impl/modules/render/displaytweaks/util/Images.java
+++ src/main/java/me/chachoox/lithium/impl/modules/render/displaytweaks/util/Images.java
@@ -4,7 +4,8 @@ public enum Images {
     PIG("lithium/textures/screen/pig.png"), //got this from swift dont ask
     BLUE_NEBULA("lithium/textures/screen/blue_nebula.png"),
     PURPLE("lithium/textures/screen/purple.png"),
-    RED_BLACK("lithium/textures/screen/red_black.png");
+    RED_BLACK("lithium/textures/screen/red_black.png"),
+    BANDHU("lithium/textures/screen/aetra_gang.png");
 
     final String image;
 
